b84ba4ab080d16f84fb41953aa0cd8629637451a
rinkuahamed <ringkuali18@gmail.com>
2023-07-26 21:18:59 +0600

    create calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function calculateNumbers(num1, num2, operator) {
+  if (operator === "+") {
+    return num1 + num2;
+  } else if (operator === "-") {
+    return num1 - num2;
+  } else if (operator === "*") {
+    return num1 * num2;
+  } else if (operator === "/") {
+    return num1 / num2;
+  } else {
+    return "no operator found";
+  }
+}
+const result = calculateNumbers(60, 20, "/");
+console.log(result);
